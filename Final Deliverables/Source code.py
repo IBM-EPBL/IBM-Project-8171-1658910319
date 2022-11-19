280fcd606507b2f9b364fa53f3966b9924094470
Sugandan S M <84927655+SugandanSM6231@users.noreply.github.com>
2022-11-19 14:53:20 +0530

    Update Source code.py

--- Final Deliverables/Source code.py
+++ Final Deliverables/Source code.py
@@ -1,35 +1,47 @@
-import wiotp.sdk.device
-import time
-import os
-import datetime
-import random
-myConfig={
-    "identity":{
-        "orgId":"uq23sr",
-        "typeId":"Smart_Farming_device",
-        "deviceId":"32856"
-    },
-    "auth": {
-        "token":"3wNLT0O1g8VpEJEpsq"
-    }
-}
-client=wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
-client.connect()
-def myCommandCallback(cmd):
-    print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
-    m=cmd.data['command']
-    if(m=="motoron"):
-        print("Motor is switched ON")
-    elif(m=="motoroff"):
-        print("Motor is switched OFF")
-    print(" ")
-while True:
-    soil=random.randint(0,100)
-    temp=random.randint(-20,125)
-    hum=random.randint(0,100)
-    myData={'soil_moisture':soil,'temperature':temp,'humidity':hum}
-    client.publishEvent(eventId="status",msgFormat="json",data=myData,qos=0,onPublish=None)
-    print("Published data successfully",myData)
-    time.sleep(2)
-    client.commandCallback=myCommandCallback
-client.disconnect()
+import time 
+import sys
+import ibmiotf.application 
+import ibmiotf.device 
+import random 
+ 
+#Provide your IBM Watson Device Credentials organization = "i3869j" deviceType = "abcd" deviceId = "12345" authMethod = "token" authToken = "12345678" 
+ 
+# Initialize GPIO 
+def myCommandCallback(cmd): 
+    print("Command received: %s" % cmd.data['command'])   
+    status=cmd.data['command']   
+    if status=="lighton":     
+        print ("led is on")     
+    elif status == "lightoff": 
+        print ("led is off")
+    else : 
+        print ("please send proper command") 
+ 
+try:*95 
+ 	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}  	deviceCli = ibmiotf.device.Client(deviceOptions) 
+ 	#.............................................. 
+ 	 
+except Exception as e: 
+ 	print("Caught exception connecting device: %s" % str(e)) 
+ 	sys.exit() 
+ 
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type 
+"greeting" 10 times deviceCli.connect() 
+ 
+while True: 
+        #Get Sensor Data from DHT11 
+         
+        temp=random.randint(90,110) 
+        Humid=random.randint(60,100) 
+         
+        data = { 'temp' : temp, 'Humid': Humid } 
+        #print data 
+        def myOnPublishCallback(): 
+            print ("Published Temperature = %s C" % temp, "Humidity = %s %%" % Humid, "to IBM Watson") 
+ 
+        success = deviceCli.publishEvent("IoTSensor", "json", data, qos=0, on_publish=myOnPublishCallback) 
+        if not success:             print("Not connected to IoTF")         time.sleep(10) 
+         
+        deviceCli.commandCallback = myCommandCallback 
+ 
+# Disconnect the device and application from the cloud deviceCli.disconnect() 
